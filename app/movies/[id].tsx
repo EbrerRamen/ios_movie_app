d72f921e1a2f25d5e9900556697e57041a277ca4
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-28 20:13:35 +0600

    Movie details page initial setup

---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import {View, Text} from "react-native";
+import React from 'react';
+
+const MovieDetails = () => {
+    return (
+        <View>
+            <text>MovieDetails</text>
+        </View>
+    )
+}
+
+export default MovieDetails;
