76c8d6a69c5abf42cd74b0a2bc70e28f3801de84
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-28 21:03:13 +0600

    Converted home button design to a general function

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,18 @@ import {images} from "@/constants/images";
 import {ImageBackground, Image, Text} from "react-native";
 import {icons} from "@/constants/icons";
 
+const TabIcon = () => {
+    return (
+        <ImageBackground
+            source={images.highlight}
+            className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+        >
+            <Image source={icons.home} tintColor="#151312" className="size-5" />
+            <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
+        </ImageBackground>
+    )
+}
+
 const _Layout = () => {
     return (
         <Tabs>
@@ -13,16 +25,7 @@ const _Layout = () => {
                     title: "Home",
                     headerShown: false,
                     tabBarIcon: ({ focused }) => (
-                        <>
-                            <ImageBackground
-                                source={images.highlight}
-                                className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
-                            >
-                                <Image source={icons.home} tintColor="#151312" className="size-5" />
-                                <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
-                            </ImageBackground>
-
-                        </>
+                        <TabIcon/>
                     )
                 }}
             />
