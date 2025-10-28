97538b60aec1a646783ea4fd9a121a8b01c43247
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-28 19:55:57 +0600

    Onboarding page setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
                 dark: {
                     100: '#221f3d',
                     200: '#0f0d23',
-                }
+                },
                 accent: '#AB8BFF'
             }
         },
