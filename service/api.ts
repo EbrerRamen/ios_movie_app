4b6450d8544ce9f9b981eedcf2fb7f3e430e016a
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-29 22:07:02 +0600

    .env deleted

--- service/api.ts
+++ service/api.ts
@@ -17,7 +17,8 @@ export const fetchPopularMovies = async ({query}: {query: string}) => {
     })
 
     if (!response.ok) {
-        throw new Error('Failed to fetch movies', response.StatusText);
+        // @ts-ignore
+        throw new Error('Failed to fetch movies', response.statusText);
     }
 
     const data = await response.json();
