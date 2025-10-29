af38d4aef257887ad8dbb961e6b1176661ea77a7
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-29 21:17:09 +0600

    Designed searchbar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import {Image, ScrollView, Text, View} from "react-native";
 import {Link} from "expo-router"
 import {images} from "@/constants/images";
 import {icons} from '@/constants/icons';
+import SearchBar from "@/components/Searchbar";
 
 export default function Index() {
   return (
@@ -10,6 +11,9 @@ export default function Index() {
 
         <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}>
             <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+            <View className="flex-1 mt-5">
+                <SearchBar />
+            </View>
         </ScrollView>
     </View>
   );
