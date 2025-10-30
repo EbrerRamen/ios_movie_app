b6082e43e119caa0a82346cd88ba6a90980c944f
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-31 00:57:21 +0600

    Fetching latest movies with movie poster

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -5,11 +5,11 @@ import {Link} from "expo-router";
 const MovieCard = ({id, poster_path, title, vote_average, release_date}: Movie) => {
   return (
         <Link href={`/movies/${id}`} asChild>
-            <TouchableOpacity className = "w-[30%">
+            <TouchableOpacity className = "w-[30%]">
                 <Image
                     source={{
                         uri: poster_path
-                        ? `https://image.tmdb.org/t/p/w55${poster_path}`
+                        ? `https://image.tmdb.org/t/p/w500${poster_path}`
                             : 'https://placehold.co/600x400/1a1a1a/ffffff.png'
                     }}
                     className={"w-full h-52 rounded-lg"}
