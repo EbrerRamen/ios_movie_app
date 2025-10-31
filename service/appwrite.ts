1ddee6294b6e8b24cae248973f9f635d69ff3f51
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-31 23:59:44 +0600

    Appwrite setup

--- service/appwrite.ts
+++ service/appwrite.ts
@@ -1,12 +1,15 @@
 // track the searches made by a user
 
-import {Client} from "react-native-appwrite";
+import {Client, Databases} from "react-native-appwrite";
 
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
-const client = new Client();
-    .setEndpoint
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1');
+    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+
+const database = new Databases(client);
 
 export const updateSearchCount = async (query: string, movie: Movie) => {
 
