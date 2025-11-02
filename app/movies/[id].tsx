7d55f37bc73520328d03f962c3b8a9ae989cc820
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-11-03 00:36:53 +0600

    Movie revenue added in movie details page

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -43,7 +43,9 @@ const MovieDetails = () => {
                     <MovieInfo label={"Genres"} value={movie?.genres?.map((g)=> g.name).join(" - ") || "N/A"} />
                     <View className={"flex flex-row justify-between w-1/2"}>
                         <MovieInfo label={"Budget"} value={`$${movie?.budget / 1_000_000} million`} />
+                        <MovieInfo label={"Revenue"} value={`$${Math.round(movie?.revenue) / 1_000_000}`} />
                     </View>
+
                 </View>
             </ScrollView>
         </View>
