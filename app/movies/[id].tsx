0cff34959d820701c5c82670f96039b5b3ff3dec
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-11-03 00:46:28 +0600

    go back button routed

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,6 +1,6 @@
 import {View, Text, ScrollView, Image, TouchableOpacity} from "react-native";
 import React from 'react';
-import {useLocalSearchParams} from "expo-router";
+import {router, useLocalSearchParams} from "expo-router";
 import {fetchMovieDetails} from "@/service/api";
 import useFetch from "@/service/useFetch";
 import {icons} from "@/constants/icons";
@@ -49,7 +49,7 @@ const MovieDetails = () => {
                 </View>
             </ScrollView>
 
-            <TouchableOpacity className={"absolute bottom-5 left-0 right-0 mx-5 bg-accent rounded-lg py-3.5 flex flex-row items-center justify-center z-50"}>
+            <TouchableOpacity className={"absolute bottom-5 left-0 right-0 mx-5 bg-accent rounded-lg py-3.5 flex flex-row items-center justify-center z-50"} onPress={router.back}>
                 <Image source={icons.arrow} className = "size-5 mr-1 mt-0.5 rotate-180" tintColor={"#fff"} />
                 <Text className={"text-white font-semibold text-base"}>Go back</Text>
             </TouchableOpacity>
