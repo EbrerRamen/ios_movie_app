82ce04e5c613df74f28415da9a6d471be35df530
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-11-03 00:23:11 +0600

    MovieInfo function for fetching different informations about the movie

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -5,6 +5,18 @@ import {fetchMovieDetails} from "@/service/api";
 import useFetch from "@/service/useFetch";
 import {icons} from "@/constants/icons";
 
+interface MovieInfoProps {
+    label: string;
+    value?: string | number | null;
+}
+
+const MovieInfo = ({label, value}: MovieInfoProps) => (
+    <View className={"flex-col items-start justify-center mt-5"}>
+        <Text className={"text-light-200 font-normal text-sm"}>{label}</Text>
+        <Text className={"text-light-100 font-bold text-sm mt-2"}>{value || 'N/A'}</Text>
+    </View>
+)
+
 const MovieDetails = () => {
 
     const {id} = useLocalSearchParams();
