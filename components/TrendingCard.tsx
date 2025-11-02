376e1abc1f2027c18b36075cf4108188b83a09fb
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-11-02 22:54:09 +0600

    Trending Movie Card initial setup

---
+++ components/TrendingCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TrendingCard = () => {
+  return (
+    <View>
+      <Text>TrendingCard</Text>
+    </View>
+  );
+};
+
+export default TrendingCard;
